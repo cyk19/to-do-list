01cd294a2b03cbb523a3efbb2df477f44ace7228
cyk19 <cyk19@uclive.ac.nz>
2019-04-28 20:32:57 +1200

    First commit

---
+++ todoscript.js
@@ -0,0 +1,67 @@
+// Create a "close" button and append it to each list item
+
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodeList[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++){
+	close[i].onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Add a "checked symbol when clicking on a list item 
+//var list = document.getElementByID('myUL'); 
+// usually get object by class
+//.addEventListener(event, function, capture(true/false) ; capture noti appears first
+//list.addEventListener('click', function(ev) { 
+//	if (ev.target.tagName === "LI") {
+//		ev.target.classList.toggle('checked'); 
+		//.toggle add something to the classlist of the object
+//	}
+//}, false);
+		
+// create new list item when clicking add button
+function newElement() {
+	var li = document.createElement("li");
+	var inputText = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputText);
+	li.appendChild(t);
+	if (inputText === '') {
+		alert("You must write something!");
+	}else {
+		document.getElementById("myUL").appendChild(li);
+	}document.getElementById("myInput").value = '';
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
